7a1c32910b2a02336a3afc8a511b825921266748
alwaysreaddocs <alwaysreaddocs@gmail.com>
2025-11-25 17:07:05 +0100

    revert to modules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const handler = require("./api/webhook.js");
+import express from "express";
+import bodyParser from "body-parser";
+import handler from "./api/webhook.js";
 
 const app = express();
 app.use(bodyParser.json());
